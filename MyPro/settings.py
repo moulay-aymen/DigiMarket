cc9a3c7052ba0a4cddfbbc9a11f442bb17af4476
moulay-aymen <moulayaymen.bouabdallah@gmail.com>
2026-02-17 20:54:06 +0100

    Update settings.py

--- MyPro/settings.py
+++ MyPro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1bwmi^7s3u$+tro%(n*qg97_*21g*1^t+k97n5+8j!4=wxt$66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

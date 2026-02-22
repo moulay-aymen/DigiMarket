8a03f1ee0a1a0fcc49ea980e9aa58f37e2903fe9
moulay-aymen <moulayaymen.bouabdallah@gmail.com>
2026-02-22 17:52:16 +0100

    Update settings.py

--- MyPro/settings.py
+++ MyPro/settings.py
@@ -121,6 +121,7 @@ import os
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
